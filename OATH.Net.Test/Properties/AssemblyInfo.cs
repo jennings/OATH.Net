5c9d458a6eb98c933fabd0108ac9f380b34f686a
Stephen Jennings <Stephen.G.Jennings@gmail.com>
2011-10-15 21:39:01 -0700

    Initial commit.

---
+++ OATH.Net.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿//------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Stephen Jennings">
+//   Copyright 2011 Stephen Jennings. Licensed under the Apache License, Version 2.0.
+// </copyright>
+//------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OATH.Net Tests")]
+[assembly: AssemblyDescription("Unit tests for OATH.Net")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Stephen Jennings")]
+[assembly: AssemblyProduct("OATH.Net")]
+[assembly: AssemblyCopyright("Copyright 2011 Stephen Jennings")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("3c7b2afb-4217-44df-99ac-f91d5102f25a")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1-g*")]
---
+++ README.md
@@ -0,0 +1,38 @@
+OATH.Net
+====================
+
+OATH.Net is a .NET library to perform OATH authentication.
+
+
+## Requirements
+
+* Microsoft .NET Framework 4.0
+
+
+## Install
+
+TODO
+
+
+## Source Code
+
+OATH.Net is on GitHub:
+
+    http://github.com/jennings/OATH.Net
+
+
+## License
+
+Copyright 2011 Stephen Jennings
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
