ae297e69694d0e86022c76d654273d1f60fac803
Stephen Jennings <Stephen.G.Jennings@gmail.com>
2011-10-21 23:41:26 -0700

    Create CommonAssemblyInfo

--- OATH.Net/Properties/AssemblyInfo.cs
+++ OATH.Net/Properties/AssemblyInfo.cs
@@ -10,14 +10,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("OATH.Net")]
 [assembly: AssemblyDescription("An open-source library for OATH authentication.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Stephen Jennings")]
-[assembly: AssemblyProduct("OATH.Net")]
-[assembly: AssemblyCopyright("Copyright 2011 Stephen Jennings")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("3c7b2afb-4217-44df-99ac-f91d5102f25a")]
-
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1-g*")]
---
+++ OATH.Net/Properties/CommonAssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿//------------------------------------------------------------------------------------
+// <copyright file="CommonAssemblyInfo.cs" company="Stephen Jennings">
+//   Copyright 2011 Stephen Jennings. Licensed under the Apache License, Version 2.0.
+// </copyright>
+//------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Stephen Jennings")]
+[assembly: AssemblyProduct("OATH.Net")]
+[assembly: AssemblyCopyright("Copyright 2011 Stephen Jennings")]
+[assembly: AssemblyConfiguration("")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1-g*")]
